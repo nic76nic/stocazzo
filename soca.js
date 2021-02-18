6728783a5d6b1cf5d95daeca449bf1d7802d399d
nic76nic <36694095+nic76nic@users.noreply.github.com>
2021-02-18 14:19:54 +0100

    Add files via upload

---
+++ soca.js
@@ -0,0 +1,15 @@
+<h1>Diomaiale</h1>
+alert('diocane');
+alert('dioboia');
+window.alert('diostrunz');
+var nome = prompt ('chi cazzo sei?');
+alert ('ciao ' + nome + ', cacati in mano');
+alert ('hai scritto ' + nome.length + ' caratteri. Puoi scrivere altre ' + (150 - nome.length) + ' cazzate');
+
+var stronzi = prompt ('dimmi un numero');
+function culo(){
+    alert('diostronzo hai cacato ' + stronzi + ' stronzi');
+    alert('diocane hai cacato ' + stronzi + ' stronzi');
+    alert('dioboia hai cacato ' + stronzi + ' stronzi');
+}
+culo(stronzi);
